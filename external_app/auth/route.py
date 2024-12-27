9c0ac1af884e61dc2ed22f9362ee86ef379e7b15
Tim <timoxaisgod@gmail.com>
2024-12-27 20:17:36 +0300

    Микроизменения

--- external_app/auth/route.py
+++ external_app/auth/route.py
@@ -24,7 +24,7 @@ def valid_authorization_request(api_request):
 def decode_basic_authorization(api_request):
     auth_header = api_request.headers.get('Authorization')
     token = auth_header.split()[-1]
-    login_and_password = b64decode(token.encode('ascii')).decode('ascii').split(':')
+    login_and_password = b64decode(token.encode('utf-8')).decode('utf-8').split(':')
     if len(login_and_password) != 2:
         raise ValueError('Invalid login and password format')
     login, password = login_and_password
